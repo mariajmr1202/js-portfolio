0ed8979a8d9745758704eefa894c9445c8d8bdb3
mariajmr1202 <mariajmr1202@gmail.com>
2021-05-03 13:12:22 -0400

    Loader Babel y plugin html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   //Punto de entrada de la app
@@ -12,4 +13,29 @@ module.exports = {
     //Extensiones con la que trabajaremos en el proyecto: .js, react, etc
     extensions: [".js"],
   },
+  module: {
+    rules: [
+      {
+        // Test declara que extensión de archivos transformara el loader, en este caso archivos .js
+        test: /\.m?js$/,
+        // Exclude permite omitir archivos o carpetas especificas, para que no escoga los archivos .js de esas carpetas
+        exclude: /node_modules/,
+        use: {
+          //Le indicamos que usaremos el Loader Babel
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    //Configuraciones plugin de HTML
+    new HtmlWebpackPlugin({
+      //inyecta el bundle al template html
+      inject: true,
+      //la ruta al template html
+      template: "./public/index.html",
+      //nombre final del archivo
+      filename: "./index.html",
+    }),
+  ],
 };
