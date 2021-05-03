e18c0d09f94916af232c5606e56c7f6e680574d4
mariajmr1202 <mariajmr1202@gmail.com>
2021-05-03 18:24:17 -0400

    Se agrego a webpack las imagenes y las fuentes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   //Punto de entrada de la app
@@ -8,6 +10,8 @@ module.exports = {
     //Lugar donde se va a guardar/preparar el proyecto
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    //Para guardar las imagenes generadas en la carpeta assets
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
     //Extensiones con la que trabajaremos en el proyecto: .js, react, etc
@@ -15,6 +19,7 @@ module.exports = {
   },
   module: {
     rules: [
+      //Reglas para babel
       {
         // Test declara que extensión de archivos transformara el loader, en este caso archivos .js
         test: /\.m?js$/,
@@ -23,6 +28,32 @@ module.exports = {
         use: {
           //Le indicamos que usaremos el Loader Babel
           loader: "babel-loader",
+        },
+      },
+      //Reglas para CSS
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+      },
+      //Reglas para imagenes
+      {
+        test: /\.png/,
+        type: "asset/resource",
+      },
+      //Reglas para fuentes
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            //Hacia que carpeta se enviara
+            outputPath: "./assets/fonts",
+            publicPath: "./assets/fonts",
+            esModule: false,
+          },
         },
       },
     ],
@@ -36,6 +67,19 @@ module.exports = {
       template: "./public/index.html",
       //nombre final del archivo
       filename: "./index.html",
+    }),
+    //Configuraciones plugin de CSS
+    new MiniCssExtractPlugin(),
+    //Configuraciones plugin para copiar/mover archivos
+    new CopyPlugin({
+      patterns: [
+        {
+          //Colocamos el archivos o carpeta que vamos a mover, en este
+          //caso assets/images, movere todas las imagenes a dist
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
     }),
   ],
 };
